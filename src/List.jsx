47a8742f141ab7808b76400519bfb159fa974156
Ashish <ashishredhu5810@gmail.com>
2024-05-24 23:32:28 +0530

    form element added at the place of simple textfield and button

--- src/List.jsx
+++ src/List.jsx
@@ -31,8 +31,10 @@ export default function List({ arr, deleteElement, editElement }) {
                    <ListItem key={index} className="task">
                        {editIndex === index ? (
                           <div className="editform">
-                            <TextField value={editText} onChange={handleEditChange} required={true} variant="filled" className="input"></TextField>
-                             <Button onClick={()=>handleEditSubmit(index)} type="submit" variant="contained" className="button"> Save </Button>
+                            <form onSubmit={()=>handleEditSubmit(index)}>
+                             <TextField value={editText} onChange={handleEditChange} required={true} variant="filled" className="input"></TextField>
+                             <Button type="submit" variant="contained" className="button"> Save </Button>
+                             </form>
                           </div>
                        ):(
                         <>
@@ -51,5 +53,11 @@ export default function List({ arr, deleteElement, editElement }) {
 
 //1.)  We can't use traditional loops in REACT. arr.map is an amazing way to traverse the arrays in REACT.
 // 2.) It is mendatory to wrap this deleteElement(index) function inside an arrow function. Else it will execute just after page rendering means nothing will be added in the list.
+
+
+// 3.) Note: To traverse array in React use "arr.map()", to remove something from array use "arr.filter()" in react.
+// 4.) Use forms even when you are taking single input.
+
+
 
 
